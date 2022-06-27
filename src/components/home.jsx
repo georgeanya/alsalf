76fe0b9e485ddcf07412b3bd04ceb45c1b5ba1f6
George Anya <georgeanya4real@gmail.com>
2022-06-27 12:16:23 +0100

    commit

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,19 +7,19 @@ const Home = () => {
     <div>
       <div className="" id="home">
         <h1 className="home_head">
-          Welcome to <br/> Abiriba Let’s Save A Life Foundation (A.L.S.A.L.F)
+          Welcome to <br /> Abiriba Let’s Save A Life Foundation (A.L.S.A.L.F)
         </h1>
         <img className="home_img" src={image1} width="100%" alt="" />
-          </div>
-          
+      </div>
+
       <div id="about">
         <h2 className="sub_head_text">About Us</h2>
         <p className="sub_body_text">
           We are a group of Abiriba indigenes driven by compassion to provide
-          the community with good healthcare support. We believe a healthy community is
-          a productive community and we are willing to go the extra mile to
-          ensure the average Abiriba indigene living in Abiriba has access to immediate assitance to
-          quality health care.
+          the community with good healthcare support. We believe a healthy
+          community is a productive community and we are willing to go the extra
+          mile to ensure the average Abiriba indigene living in Abiriba has
+          access to immediate assitance to quality health care.
         </p>
       </div>
       <div id="aim">
@@ -28,9 +28,10 @@ const Home = () => {
         </h2>
         <p className="sub_body_text">
           We are targeted at helping members of the community by providing early
-          detection of ailments, providing advise and support to prevent these ailments
-          from getting out of control and also providing aftercare support to
-          these people.
+          detection of ailments, providing advise and support to prevent these
+          ailments from getting out of control and also providing aftercare
+          support to these people.<br/>For us to achive this we need far and near
+          Abriba Indigenes to come together to provide support.
         </p>
       </div>
       {/* <div id="founders">
